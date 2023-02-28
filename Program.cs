b761cf32fe182719aedb036094673487139ca84a
Subhash Barela <barelasubhash16@gmail.com>
2023-02-28 17:29:35 +0530

    find out anagram world in string

--- Program.cs
+++ Program.cs
@@ -11,16 +11,11 @@ namespace SearchAndSortTechnique
     {
         public static void Main(string[] args) 
         {
-            int []a = { 12, 31, 25, 8, 32, 17, 40, 42 };
-            int n = a.Length;
-            MergeSorting obj=new MergeSorting();
-            Console.WriteLine("Before sorting array elements are : \n");
-            obj.printArray(a, n);
-            obj.mergeSort(a, 0, n - 1);
-            Console.WriteLine("\n\nAfter sorting array elements are :\n");
-            obj.printArray(a, n);
+            AnagramProgram Obj= new AnagramProgram();
+            Obj.anagram();
+           
             Console.Read();
-            return;
+           
              
         }
 
